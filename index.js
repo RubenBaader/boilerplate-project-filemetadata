67a5588b796657bb2395c795beb6603d4ac050e9
RubenBaader <rubenbaader@gmail.com>
2023-02-22 19:53:06 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  res.json({MSG : "HELLO World!"})
+});
 
 
 
