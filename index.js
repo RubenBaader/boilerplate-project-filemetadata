f39c98305c9c0411b30da329aa280a407312e380
RubenBaader <rubenbaader@gmail.com>
2023-02-22 20:26:14 +0100

    Tiny refactor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
@@ -18,6 +18,6 @@ app.post('/api/fileanalyse', (req, res) => {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
 });
